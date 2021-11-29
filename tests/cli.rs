7b9af1d6ad1f0e97f3d6e03ececbd467ca705620
angelovangel <aangeloo@gmail.com>
2021-11-29 14:43:45 +0100

    modify cli test

--- tests/cli.rs
+++ tests/cli.rs
@@ -5,14 +5,14 @@ use std::process::Command; // Run programs
 
 #[test]
 
-fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+fn find_content_in_output() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut cmd = Command::cargo_bin("fastkmers")?;
     cmd.arg("-k 3").arg("tests/test.fastq");
 
     cmd.assert()
         .success()
-        .stdout(predicate::str::contains("CGT \t 6"));
+        .stdout(predicate::str::contains("ATC \t 10"));
 
     Ok(())
 }
