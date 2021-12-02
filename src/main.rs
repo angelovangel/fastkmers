ffd0aa49db2b48352d3232ba11dbd295e65a1f8b
angelovangel <aangeloo@gmail.com>
2021-12-02 11:28:10 +0100

    code formatting

--- src/main.rs
+++ src/main.rs
@@ -88,43 +88,42 @@ fn main() {
     let mut kmers: i64 = 0;
     
     if matches.is_present("fasta") {
-    let reader = fasta::Reader::new(get_fastq_reader(&infile));
+        let reader = fasta::Reader::new(get_fastq_reader(&infile));
 
-    for result in reader.records(){
-        reads += 1;
+        for result in reader.records(){
+            reads += 1;
 
-        let record = result.expect("Error");
-        let seq = record.seq();
-        let seq_str = str::from_utf8(seq).unwrap().to_string();
-        //println!("{:?}", seq);
-        for c in 0..seq_str.len() - k + 1 {
-            let subseq = &seq_str[c..c + k];
+            let record = result.expect("Error");
+            let seq = record.seq();
+            let seq_str = str::from_utf8(seq).unwrap().to_string();
+            //println!("{:?}", seq);
+            for c in 0..seq_str.len() - k + 1 {
+                let subseq = &seq_str[c..c + k];
             
-            kmers += subseq.len() as i64;
+                kmers += subseq.len() as i64;
         
-            *kmer_counts.entry(subseq.to_string() ).or_insert(0) += 1;
+                *kmer_counts.entry(subseq.to_string() ).or_insert(0) += 1;
             }
+        }
+    } else {
+        let reader = fastq::Reader::new(get_fastq_reader(&infile));
 
-    }
-} else {
-    let reader = fastq::Reader::new(get_fastq_reader(&infile));
+        for result in reader.records(){
+            reads += 1;
 
-    for result in reader.records(){
-        reads += 1;
-
-        let record = result.expect("Error");
-        let seq = record.seq();
-        let seq_str = str::from_utf8(seq).unwrap().to_string();
-        //println!("{:?}", seq);
-        for c in 0..seq_str.len() - k + 1 {
-            let subseq = &seq_str[c..c + k];
+            let record = result.expect("Error");
+            let seq = record.seq();
+            let seq_str = str::from_utf8(seq).unwrap().to_string();
+            //println!("{:?}", seq);
+            for c in 0..seq_str.len() - k + 1 {
+                let subseq = &seq_str[c..c + k];
             
-            kmers += subseq.len() as i64;
+                kmers += subseq.len() as i64;
         
-            *kmer_counts.entry(subseq.to_string() ).or_insert(0) += 1;
+                *kmer_counts.entry(subseq.to_string() ).or_insert(0) += 1;
             }
+        }
     }
-}
 
     if matches.is_present("json") {
         let j = serde_json::to_string(&kmer_counts).unwrap();
