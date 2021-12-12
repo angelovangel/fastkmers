42c71a501a4a4fe932e83dca34ff84b6c2d1fa2b
angelovangel <aangeloo@gmail.com>
2021-12-12 22:23:38 +0100

    fix failing test

--- tests/cli.rs
+++ tests/cli.rs
@@ -20,7 +20,7 @@ fn test_kmer_fastq() -> Result<(), Box<dyn std::error::Error>> {
 fn test_kmer_fasta() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut cmd = Command::cargo_bin("fastkmers")?;
-    cmd.arg("-k 3").arg("--fasta").arg("tests/test.fasta");
+    cmd.arg("-k 3").arg("tests/test.fasta");
 
     cmd.assert()
         .success()
@@ -47,7 +47,7 @@ fn test_regex() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("fastkmers")?;
     
     cmd.arg("-k 5").arg("-q").arg("A[T|G]A$").arg("tests/test.fastq");
-    cmd.assert().success().stdout(predicate::str::contains("AAAGA\t3"));
+    cmd.assert().success().stdout(predicate::str::contains("GAAGA\t3"));
 
     Ok(())
 
